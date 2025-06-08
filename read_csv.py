7c8edacffe5f58e542597d50a0aa1fed09330d51
wingwing99 <59636924+wingwing99@users.noreply.github.com>
2025-06-08 23:04:30 +0900

    Update read_csv.py
    파일을 정제해서 년도.csv로 변환해서 저장

--- read_csv.py
+++ read_csv.py
@@ -38,9 +38,35 @@ def clean_data(df, delete_columns, decision_columns):
     
     return df_cleaned
 
+def preprocess_data_and_save(year):
+    df_sel_year = read_year_data(year)
+
+    # 데이터 정제를 위한 기준 칼럼 정의
+    decision_column_str = ["지번구분", "토지면적(㎡)", "층", "건물용도","건축년도","건물명", "본번", "부번", "자치구명"]
+
+    # 불필요 칼럼 정의
+    delete_column_str = {"취소일", "권리구분", "신고구분", "신고한 개업공인중개사 시군구명"}
+
+    # 데이터 정제 전 결측치 분석
+    print("\n=== 정제 전 데이터 분석 ===")
+    analyze_missing_data(df_sel_year)
+
+    # 데이터 정제
+    df_sel_year_cleaned = clean_data(df_sel_year, delete_column_str, decision_column_str)
+
+    # 정제 후 결측치 분석
+    print("\n=== 정제 후 데이터 분석 ===")
+    analyze_missing_data(df_sel_year_cleaned)
+
+    # 정제된 데이터를 CSV 파일로 저장
+    output_path = str(year) + '.csv'
+    df_sel_year_cleaned.to_csv(data_path + output_path, index=False, encoding='cp949')
+    print(f"\n정제된 데이터가 {output_path}에 저장되었습니다.")
+
+    return df_sel_year_cleaned
 
 # 2017년부터 2025년까지의 데이터만 처리
-for year in range(2017, 2026):
+""" for year in range(2017, 2026):
     file_name = file_name_temp.format(year)
     file_path = data_path + file_name
     
@@ -55,24 +81,28 @@ for year in range(2017, 2026):
         except Exception as e:
             print(f"파일 처리 중 오류 발생: {e}")
     else:
-        print(f"\n{year}년 데이터 파일이 존재하지 않습니다.")
+        print(f"\n{year}년 데이터 파일이 존재하지 않습니다.") """
 
+print("===============================================")
+print("데이터 정제 시작")
+print("===============================================")
 
-df_2017 = read_year_data(2017)
+# 2017년 데이터 처리
+df_2017_cleaned = preprocess_data_and_save(2017)
 
-# 데이터 정제를 위한 기준 칼럼 정의
-decision_column_str = ["지번구분", "토지면적(㎡)", "층", "건물용도","건축년도"]
+df_2018_cleaned = preprocess_data_and_save(2018)
 
-# 불필요 칼럼 정의
-delete_column_str = {"취소일", "권리구분", "신고구분", "신고한 개업공인중개사 시군구명"}
+df_2019_cleaned = preprocess_data_and_save(2019)
 
-# 데이터 정제 전 결측치 분석
-print("\n=== 정제 전 데이터 분석 ===")
-analyze_missing_data(df_2017)
+df_2020_cleaned = preprocess_data_and_save(2020)
 
-# 데이터 정제
-df_2017_cleaned = clean_data(df_2017, delete_column_str, decision_column_str)
+df_2021_cleaned = preprocess_data_and_save(2021)
 
-# 정제 후 결측치 분석
-print("\n=== 정제 후 데이터 분석 ===")
-analyze_missing_data(df_2017_cleaned)
+df_2022_cleaned = preprocess_data_and_save(2022)
+
+df_2023_cleaned = preprocess_data_and_save(2023)
+
+df_2024_cleaned = preprocess_data_and_save(2024)
+
+df_2025_cleaned = preprocess_data_and_save(2025)
+
